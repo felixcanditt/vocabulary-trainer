f90834a0362c1c42be55f22596998bc8cbfe63d6
Felix Canditt <felix.canditt@gmail.com>
2025-08-18 17:48:51 +0200

    created 3 lists

--- src/components/VocabularyList.tsx
+++ src/components/VocabularyList.tsx
@@ -3,13 +3,40 @@ import { VocabularyItem } from '../App';
 const VocabularyList: React.FC<{ vocabularyList: VocabularyItem[] }> = (
   props
 ) => {
+  const stapleOne = props.vocabularyList.filter(
+    (item) => item.currentStaple === 1
+  );
+  const stapleTwo = props.vocabularyList.filter(
+    (item) => item.currentStaple === 2
+  );
+  const stapleThree = props.vocabularyList.filter(
+    (item) => item.currentStaple === 3
+  );
+
   return (
     <>
       <h2>list</h2>
+      <h3>staple 1</h3>
       <ul>
-        {props.vocabularyList.map((item) => (
+        {stapleOne.map((item) => (
           <li key={item.id}>
-            {item.frontText}, {item.backText}
+            {item.frontText}, {item.backText}, {item.currentStaple}
+          </li>
+        ))}
+      </ul>
+      <h3>staple 2</h3>
+      <ul>
+        {stapleTwo.map((item) => (
+          <li key={item.id}>
+            {item.frontText}, {item.backText}, {item.currentStaple}
+          </li>
+        ))}
+      </ul>
+      <h3>staple 3</h3>
+      <ul>
+        {stapleThree.map((item) => (
+          <li key={item.id}>
+            {item.frontText}, {item.backText}, {item.currentStaple}
           </li>
         ))}
       </ul>
